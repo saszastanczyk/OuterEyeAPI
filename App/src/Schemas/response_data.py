8c5f1e320659bed245e84f260c355fe7830d9701
JAYSAhh <131014076+JAYSAhh@users.noreply.github.com>
2026-07-31 14:55:48 +0300

    feat: response for plugin

---
+++ App/src/Schemas/response_data.py
@@ -0,0 +1,37 @@
+from typing import List, Optional, Dict
+
+from pydantic import BaseModel
+
+from src.Schemas.user_data import ActionData
+
+class HeaderData(BaseModel):
+    h: str #header
+    d: str #description
+
+class EnhancementData(BaseModel):
+    t: str #enhancement type
+    l: int #enhancement level
+
+class ItemData(BaseModel):
+    n: Optional[str] #name og item
+    t: str #item type
+    d: Optional[str] #item description
+    e: Optional[List[EnhancementData]] #list of enhancements
+
+class EntityData(BaseModel):
+    t:str #entity type
+    r: Optional['EntityData'] #rider data
+    e: Optional[List[ItemData]] #equipment
+    p: List[int] #entity summon position
+    a: int #amount of entities to summon
+
+class AIResponse(BaseModel):
+    b: Optional[List[Dict[str,List[int]]]] #list of blocks to place (dict of block type as key and list of positions where blocks og this type should be pasted)
+    e: Optional[List[EntityData]] #list of entities to summon
+    i: Optional[List[ItemData]] #list of items to give to a player
+    t: Optional[List[int]] #position a player should be teleported to
+    r: str #little review of player's action and description of god's will
+    h: HeaderData #data of message which plugin shows on performing god's will
+
+if __name__ == '__main__':
+    print(AIResponse.model_json_schema())
--- App/src/open_ai.py
+++ App/src/open_ai.py
@@ -3,6 +3,7 @@ import os
 from openai import AsyncOpenAI
 from App.src.Schemas.requests import PrayResponseRequest
 from src.Schemas.user_data import UserData
+from src.Schemas.response_data import AIResponse
 
 secret_key = os.environ.get("DEEPSEEK_SECRET_KEY")
 pray_prompt = os.environ.get("DEEPSEEK_PRAY_PROMPT")
@@ -23,13 +24,15 @@ async def get_pray_response(request:PrayResponseRequest) -> str:
     )
     return response.choices[0].message.content
 
-async def get_analysis_response(data:UserData) -> str:
+async def get_analysis_response(data:AIResponse) -> str:
     response = await client.chat.completions.create(
         model="deepseek-chat",
         messages=[
             {"role": "system", "content": analysis_prompt},
+            {"role": "system", "content": "Schema of your response:" + str(AIResponse.model_json_schema())},
             {"role": "user", "content": data.model_dump_json()}
         ],
-        temperature=0.1
+        temperature=0.3,
+        response_format={"type": "json_object"}
     )
     return response.choices[0].message.content
